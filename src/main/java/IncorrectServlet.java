cf2be890b62c61b0977998e61948231c06c64b91
Jordan Felan <felan.jordan94@gmail.com>
2021-07-06 09:20:41 -0500

    guess exercise modified

--- src/main/java/IncorrectServlet.java
+++ src/main/java/IncorrectServlet.java
@@ -9,7 +9,7 @@ public class IncorrectServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         request.setAttribute("result", "INCORRECT");
-        request.getRequestDispatcher("/result.jsp").forward(request, response);
+        request.getRequestDispatcher("/results.jsp").forward(request, response);
     }
 
 }
